bc1346b92b1a4b0f10e17d0f6cad24bb4fb36313
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2021-06-12 15:16:25 -0300

    Adds background, parallax and camera follow player

---
+++ KomPanyon/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] private PlayerController m_PlayerController;
+    [SerializeField] private Vector2 m_CameraOffset = new Vector2(0, 1.5f);
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float targetY = m_PlayerController.transform.position.y < 0 ? m_CameraOffset.y : m_PlayerController.transform.position.y + m_CameraOffset.y;
+        transform.position = new Vector3(
+            m_PlayerController.transform.position.x + m_CameraOffset.x,
+            targetY,
+            transform.position.z);
+
+
+    }
+}
---
+++ KomPanyon/Assets/Parallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float m_Length, m_StartPos;
+    [SerializeField] private GameObject m_Cam;
+    [SerializeField] private float m_ParallaxEffect;
+
+    void Start()
+    {
+        m_StartPos = transform.position.x;
+        m_Length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movedFromCamera = m_Cam.transform.position.x * (1- m_ParallaxEffect);
+        float dist = m_Cam.transform.position.x * m_ParallaxEffect;
+        transform.position = new Vector3(m_StartPos + dist, transform.position.y, transform.position.z);
+
+        if (movedFromCamera > m_StartPos + m_Length)
+            m_StartPos += m_Length;
+        else if (movedFromCamera < m_StartPos - m_Length)
+            m_StartPos -= m_Length;
+    }
+}
