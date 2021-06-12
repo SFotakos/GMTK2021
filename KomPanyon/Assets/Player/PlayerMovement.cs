0687a24a9538de6f0b33dfe66d33883f02557a86
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2021-06-12 16:10:19 -0300

    Adds dodge cooldown

--- KomPanyon/Assets/Player/PlayerMovement.cs
+++ KomPanyon/Assets/Player/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     bool m_ShouldJump = false;
     bool m_ShouldDodge = false;
 
+    private float m_DodgeElapsedTime;
+    [SerializeField] private float m_DodgeCooldown = 0.5f;
+
     void Update()
     {
         m_HorizontalMovement = Input.GetAxisRaw("Horizontal") * m_PlayerSpeed;
@@ -18,9 +21,16 @@ public class PlayerMovement : MonoBehaviour
             m_ShouldJump = true;
         }
 
-        if (Input.GetButtonDown("Dodge"))
+        if (m_DodgeElapsedTime > m_DodgeCooldown)
         {
-            m_ShouldDodge = true;
+            if (Input.GetButtonDown("Dodge"))
+            {
+                m_ShouldDodge = true;
+                m_DodgeElapsedTime = 0f;
+            }
+        } else
+        {
+            m_DodgeElapsedTime += Time.deltaTime;
         }
     }
 
