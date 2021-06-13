a4ef86d4cb1af08a633c9c00d98083024b5dff2f
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2021-06-13 04:23:17 -0300

    Adds basic victory flag

---
+++ KomPanyon/Assets/Environment/Flag/FlagController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Kom"))
+        {
+            Debug.Log("Next Scene");
+        }
+    }
+}
