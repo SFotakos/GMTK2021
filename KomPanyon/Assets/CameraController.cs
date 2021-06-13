d9b11b82d5d48a968038997a7b61355408e38042
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2021-06-13 03:08:36 -0300

    Fix camera flicker when switching direction

--- KomPanyon/Assets/CameraController.cs
+++ KomPanyon/Assets/CameraController.cs
@@ -9,12 +9,12 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Vector2 m_CameraOffset = new Vector2(0, 1.5f);
     
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float targetY = m_PlayerController.transform.position.y < 0 ? m_CameraOffset.y : m_PlayerController.transform.position.y + m_CameraOffset.y;
         transform.position = new Vector3(
-            m_PlayerController.transform.position.x + m_CameraOffset.x,
-            targetY,
+            (m_PlayerController.transform.position.x + m_CameraOffset.x) * 50f* Time.fixedDeltaTime,
+            targetY * 50f * Time.fixedDeltaTime,
             transform.position.z);
 
 
